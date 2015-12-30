82d29d7c5dd9463f1ebdb05cb810af3a50b07477
Toshiba <Toshiba@Toshiba-TOSH>
2015-12-30 18:01:10 +0000

    Merge origin/master
    Conflicts:
    	Client-Taxis/src/Cliente/Client.java

--- Client-Taxis/src/Cliente/Client.java
+++ Client-Taxis/src/Cliente/Client.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,25 +19,29 @@ import java.net.Socket;
  */
 public class Client implements Facade {
 
-    private Socket user;
+    private Socket server;
     BufferedReader in;
     PrintWriter out;
 
     public Client(String ip,int port) throws IOException, myException {
         try {
-            user = new Socket("localhost", 2000);
+            server = new Socket("localhost", 2000);
         } catch (java.net.ConnectException a) {
             throw new myException("Servidor não está disponivel");
         }
-        in = new BufferedReader(new InputStreamReader(user.getInputStream()));
-        out = new PrintWriter(user.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+        out = new PrintWriter(server.getOutputStream(), true);
     }
 
     @Override
     public Boolean loginPassageiro(String username,String password) throws myException {
         out.print(1+","+username+","+password+"");
         out.flush();
-        return true;
+        try {
+            in.read();
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
